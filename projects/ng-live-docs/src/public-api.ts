da3b1700c44e808f09d584f15d60bf86f047a6a8
ps37 <prudhvi.af121@gmail.com>
2020-04-01 12:25:49 -0400

    Add the Ng-live-docs sub project

---
+++ projects/ng-live-docs/src/public-api.ts
@@ -0,0 +1,19 @@
+/*!
+ * Copyright 2020 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/*
+ * Public API Surface of doc-lib
+ */
+
+export * from './documentation-retriever.service';
+export * from './documentation';
+export * from './compodoc/compodoc-schema';
+export * from './ng-live-docs.module';
+export * from './api-viewer';
+export * from './documentation-container';
+export * from './example-viewer';
+export * from './overview-viewer';
+export * from './source-code-viewer';
+export * from './stack-blitz-writer.service';
