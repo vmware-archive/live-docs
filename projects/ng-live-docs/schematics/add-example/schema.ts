280333639cd4c84f6cab246dfb15f8ce00a23876
yumengwu <yumengwu95@gmail.com>
2020-11-09 08:36:52 -0500

    Add a generation schematics `add-example` to `@vmw/ng-live-docs` to make it easy to create examples for the entity details container
    Signed-off-by: yumengwu <yumengwu95@gmail.com>

---
+++ projects/ng-live-docs/schematics/add-example/schema.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright 2020 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+export interface Schema {
+    componentName: string;
+    exampleName: string;
+    displayName: string;
+}
