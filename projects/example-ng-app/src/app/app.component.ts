60a33bf556911e2ea4d9e3e04face90b4d91316e
Juan Mendes <jmendes@vmware.com>
2020-12-29 13:20:08 -0500

    Remove deprecated props in (Example|Documentation)Entry
    These props were needed when we used decorators to register examples but
    we don't use decorators anymore
    
    Signed-off-by: Juan Mendes <jmendes@vmware.com>
    Fixes #14
    Signed-off-by: Juan Mendes <jmendes@vmware.com>

--- projects/example-ng-app/src/app/app.component.ts
+++ projects/example-ng-app/src/app/app.component.ts
@@ -4,18 +4,17 @@
  */
 
 import { Component } from '@angular/core';
-import { ApiViewerComponent, ExampleEntry } from '@vmw/ng-live-docs';
+import { ExampleEntry } from '@vmw/ng-live-docs';
 import { ApiViewerExampleComponent } from '../example-components/apiviewer/api-viewer.example.component';
 
 @Component({
-  selector: 'vmw-examples-app',
-  templateUrl: './app.component.html'
+    selector: 'vmw-examples-app',
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  componentDescriptor: ExampleEntry = {
-    component: ApiViewerExampleComponent,
-    forComponent: ApiViewerComponent,
-    title: 'Api viewer',
-    urlSegment: 'api-viewer'
-  };
+    componentDescriptor: ExampleEntry = {
+        component: ApiViewerExampleComponent,
+        title: 'Api viewer',
+        urlSegment: 'api-viewer',
+    };
 }
