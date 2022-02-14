60d5b8e67e833638f07f535251209aec43156d79
Juan Mendes <mendejuan@gmail.com>
2022-02-14 16:17:36 -0500

    Automated changes for Angular upgrade
    Signed-off-by: Juan Mendes <mendejuan@gmail.com>

--- projects/plain-js-live-docs/src/test.ts
+++ projects/plain-js-live-docs/src/test.ts
@@ -5,8 +5,8 @@
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 // The two zone.js imports below must be loaded before anything else
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
+import 'zone.js';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 declare const require: any;
